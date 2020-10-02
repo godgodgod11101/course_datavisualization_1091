b45edf4776d755c81933b0597e7c2c3f392fd65d
godgodgod11101 <a008502506@gmail.com>
2020-10-02 09:17:51 +0800

    20201002 class
    json

--- create_directory.R
+++ create_directory.R
@@ -1,11 +1,11 @@
 # 在R project底下建立“hw”資料夾（此文件需存在與project同層）
-dir.create("hw")
+dir.create("./hw")
 
 
 # 在“hw”內建立“rData”資料夾
-frontPath <- "hw"
+frontPath <- "./hw"
 folderName <- "rData"
-fullPath <- file.path(frontPath, folderName)
+fullPath <- file.path(frontPath, folderName)    # 形成R可識別的路徑
 dir.create(fullPath)
 
 # 若 “hw/rData” 路徑不存在
@@ -14,3 +14,7 @@ dir.create(fullPath, recursive = T)
 
 
 # 在“hw”內建立“rGraph”與“rSVG”資料夾
+dirname(fullPath)    # exclude the last path separator
+vec_fullPath <- file.path(dirname(fullPath), c("rGraph", "rSVG"))
+purrr::map(vec_fullPath, ~{dir.create(.x)})
+
